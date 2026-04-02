5e4f929341fc2521e205f0177056283a35432de7
Udoy Munna <udoymunna17@gmail.com>
2026-04-02 17:30:11 +0600

    Implement visibility toggle for elements and add event listener for "Get Started" button and Add functionality for "Get Started" button to validate user input and reveal hidden sections

---
+++ script/get-in.js
@@ -0,0 +1,35 @@
+// Call the EventListener if the "Get Started" button clicked.
+
+document.getElementById("get-in-btn")
+    .addEventListener('click', function(event) {
+        event.preventDefault();
+
+        // Get the name form the given Form-------
+        const name = document.getElementById("name-field").value;
+
+        // Get the login Code form the given Form ----
+        const password = document.getElementById("password-field").value;
+
+
+        // Name and Password Validation .............
+        if (name.length !== 0) {
+            if (password === "123456") {
+                const hideElements = document.querySelectorAll('.hide-me');
+
+                hideElements.forEach(el => {
+                    el.classList.remove('hide-me');
+                })
+
+                document.getElementById("name-field").value = '';
+                document.getElementById("password-field").value = '';
+                
+            }
+            else {
+                alert("Enter the Correct Login Code...");
+            }
+        }
+        else {
+            alert("Please, Enter your Name Correctly..");
+        }
+
+    })
